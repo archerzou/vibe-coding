4dd765801c7f7e3bc7acaf6d8080a4a22fabda27
Zou Zhefeng <archer.zou84@gmail.com>
2025-07-13 01:01:50 +1200

    create e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import {getSandbox} from "@/inngest/utils";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async () => {
+            const sandbox = await Sandbox.create("vibe-nextjs-archerzou");
+            return sandbox.sandboxId;
+        })
+
         // Create a new agent with a system prompt (you can add optional tools, too)
         const codeAgent = createAgent({
             name: "code-agent",
@@ -15,8 +22,14 @@ export const helloWorld = inngest.createFunction(
         const { output } = await codeAgent.run(
             `Write the following snippet: ${event.data.value}`,
         );
-        console.log(output);
 
-        return { output };
+        const sandboxUrl = await step.run("get-sandbox-url", async () => {
+            const sandbox = await getSandbox(sandboxId);
+            const host = sandbox.getHost(3000);
+
+            return `https://${host}`;
+        })
+
+        return { output, sandboxUrl };
     },
 );
